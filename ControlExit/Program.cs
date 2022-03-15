1ad8334e3c5c81c1c25945b2480f63a8d68c6438
Михаил Денисов <migelius.invesy@gmail.com>
2022-03-15 18:24:21 +0300

    Добавьте файлы проекта.

---
+++ ControlExit/Program.cs
@@ -0,0 +1,20 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        string exitCommand;
+        
+        Console.Write("Добро пожаловать в наш бар!");
+        exitCommand = Console.ReadLine();
+
+        while (exitCommand != "exit")
+        {
+           Console.WriteLine("Бухаем!");
+           exitCommand = Console.ReadLine();
+            if (exitCommand == "exit")
+            {
+                break;
+            }
+        }
+    }
+}
